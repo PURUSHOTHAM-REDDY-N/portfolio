cebea58ab671096e02f3952cc86acd685d2064f1
PURUSHOTHAM REDDY <84611950+PURUSHOTHAM-REDDY-N@users.noreply.github.com>
2022-11-22 13:43:47 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -42,7 +42,7 @@ export const projectData = [
             `Beautiful and Responsive UI design.`,
             'Full functionalities without need of any backend.',
         ],
-        outlookImg: '/images/projectImages/outlook/profile.jfif',
+        outlookImg: '/images/projectImages/outlook/issue.jpeg',
 //         images: [
 //             process.env.PUBLIC_URL + '/images/projectImages/homeprofile.png',
 //             process.env.PUBLIC_URL + '/images/projectImages/aboutprofile.png',
