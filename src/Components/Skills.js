165df6f7da229fd93f77fd595beb09595dc40c3b
PURUSHOTHAM-REDDY-N <84611950+PURUSHOTHAM-REDDY-N@users.noreply.github.com>
2023-12-23 21:56:37 +0530

    packages updated and added framerjs for animations

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,55 +1,61 @@
-import React from 'react'
-import styles from './skills.module.css'
-import { skillsData } from '../data/skillsData'
+import React from "react";
+import styles from "./skills.module.css";
+import { skillsData } from "../data/skillsData";
+import { motion } from "framer-motion";
+
 // import image from '../images/skillsData/javascript.png'
 const Skills = () => {
-    let key = 1
-    let slide = 0
-    return (
-        <div className='container'>
-            <div className={styles.title}>
-                <div data-aos-delay='300' data-aos='fade-left'>
-                    I can work with.....
-                </div>
-                <div data-aos='fade-left' className={styles.bottomborder}></div>
-            </div>
-            <div className={styles.skill_list}>
-                {skillsData.map((ele) => {
-                    key = key + 1
-                    slide++
-                    return (
-                        <div
-                            data-aos={
-                                slide % 2 === 0 ? 'slide-right' : 'slide-left'
-                            }
-                            className={styles.skill_container}
-                            key={key}
-                        >
-                            <div className={styles.skilltitle}>{ele.title}</div>
-                            <hr />
-                            <div className={styles.skilldetail}>
-                                {ele.techStack.map((tech) => {
-                                    key = key + 1
-                                    return (
-                                        <div className={styles.tech} key={key}>
-                                            <img
-                                                className={styles.tech_img}
-                                                src={tech.image}
-                                                alt={tech.title}
-                                            />
-                                            <div className={styles.tech_title}>
-                                                {tech.title}
-                                            </div>
-                                        </div>
-                                    )
-                                })}
-                            </div>
-                        </div>
-                    )
-                })}
-            </div>
+  let key = 1;
+  let slide = 0;
+  return (
+    <div className="container">
+      <div className={styles.title}>
+        <div data-aos-delay="300" data-aos="fade-left">
+          I can work with.....
         </div>
-    )
-}
+        <div data-aos="fade-left" className={styles.bottomborder}></div>
+      </div>
+      <div className={styles.skill_list}>
+        {skillsData.map((ele) => {
+          key = key + 1;
+          slide++;
+          return (
+            <div
+              data-aos={slide % 2 === 0 ? "slide-right" : "slide-left"}
+              className={styles.skill_container}
+              key={key}
+            >
+              <div className={styles.skilltitle}>{ele.title}</div>
+              <hr />
+              <div className={styles.skilldetail}>
+                {ele.techStack.map((tech) => {
+                  key = key + 1;
+                  return (
+                    <motion.a
+                      whileTap="tap"
+                      whileHover={{
+                        scale: 1.2,
+                        transition: { duration: 1 },
+                      }}
+                    >
+                      <div className={styles.tech} key={key}>
+                        <img
+                          className={styles.tech_img}
+                          src={tech.image}
+                          alt={tech.title}
+                        />
+                        <div className={styles.tech_title}>{tech.title}</div>
+                      </div>
+                    </motion.a>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Skills
+export default Skills;
